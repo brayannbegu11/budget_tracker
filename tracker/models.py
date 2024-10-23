855dea7fc774747e3e3e35ec66eff2312a3b97ef
Brayann Benavides <brayannbegu11@gmail.com>
2024-10-23 10:25:00 -0400

    Define the models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User # This lets us associate incomes/expenses with users
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+    
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class Income(models.Models):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True)
+    description = models.TextField(blank=True, null = True) #Optional description
+    date = models.DateField()
+    
+    def __str__(self):
+        return f'{self.amount} - {self.category}'
+    
+class Expense(models.Models):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True)
+    description = models.TextField(blank=True, null = True) #Optional description
+    date = models.DateField()
+    
+    def __str__(self):
+        return f'{self.amount} - {self.category}'
