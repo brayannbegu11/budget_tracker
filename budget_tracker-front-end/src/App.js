95bf5c562e220a3ddc3f4872faf6ada5cdc54d85
Brayann Benavides <brayannbegu11@gmail.com>
2024-10-23 13:39:08 -0400

    Quick fixes routing from v5 to v6

--- budget_tracker-front-end/src/App.js
+++ budget_tracker-front-end/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/login' Component={Login} />
-      </Switch>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </Router>
   );
 }
