7b6cacd357d77b3a4b593e17683157e7fd9e98bd
Brayann Benavides <brayannbegu11@gmail.com>
2024-10-23 13:30:28 -0400

    Adding login page

---
+++ budget_tracker-front-end/src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
+
+export const Login = () => {
+    const {login} = useAuth()
+    const [credentials, setCredentials] = useState({username: '', password:''})
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setCredentials({...credentials, [name]: value})
+    }
+
+    const handleSubmit = async(e) => {
+        e.prevent.default()
+        await (login(credentials))
+    }
+  return (
+    <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder='Username' value={credentials.username} onChange={handleChange} required />
+        <input type="password" name="password" placeholder='Password' value={credentials.password} onChange={handleChange} required />
+        <button type="submit">Login</button>
+    </form>
+  )
+}
